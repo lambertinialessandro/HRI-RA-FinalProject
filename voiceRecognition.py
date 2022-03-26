fffd56b4a4608b4d6c808f42bf3f0b2fd7707acc
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-03-26 11:31:12 +0100

    Create voiceRecognition.py

---
+++ voiceRecognition.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 25 13:02:58 2022
+
+@author: lambe
+"""
+
+# pip install speechRecognition
+# pip install pyttsx3
+
+# pip install pipwin
+# pipwin install pyaudio
+
+import speech_recognition as sr
+import pyttsx3
+
+name = "tello"
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    command = ''
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            print(command)
+            if name in command:
+                command = command.replace(name, '')
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print("Executing: "+command)
+    if 'start' in command:
+        talk("Starting drone! wrwrwr")
+
+    elif 'stop' in command:
+        talk("Stopping drone! wrwrwr")
+
+    elif 'follow me' in command:
+        talk("i'm following")
+
+    elif 'turn off' in command:
+        talk("Stop execution")
+        return False
+
+    else:
+        talk('Please say the command again.')
+
+    return True
+
+state = True
+while state:
+    state = run_alexa()
