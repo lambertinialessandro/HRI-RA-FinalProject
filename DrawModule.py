7a4b8f31cf714088016d382594b7d9f061564ef7
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-03-27 17:51:54 +0200

    module for plot functions

---
+++ DrawModule.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 27 16:01:48 2022
+
+@author: lambe
+"""
+
+import time
+import cv2
+import math
+
+from HandEnum import HandEnum
+
+
+# Draw FPS on the top left corner
+def drawFps(obj):
+    obj.pTime = 0
+    obj.cTime = 0
+
+    def f(self, img, drawFps=False, drawFpsColor=(255, 0, 255), **kwargs):
+        if drawFps:
+            self.cTime = time.time();
+            fps = 1/(self.cTime - self.pTime)
+            self.pTime = self.cTime
+
+            cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                        drawFpsColor, 3)
+        return img
+    return f
+
+# Draw hand connections
+def drawHand(obj):
+    def f(self, img, drawHand=False, **kwargs):
+        if self.resultsData and drawHand:
+            for handLms in self.results.multi_hand_landmarks:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+        return img
+    return f
+
+# Draw Bbox
+def drawBbox(obj):
+    def f(self, img, drawBbox=False, drawBboxColor=(255, 0, 255), **kwargs):
+        if self.resultsData and drawBbox:
+            for hand in self.allHands:
+                bbox = hand["bbox"]
+                cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                              (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                              drawBboxColor, 2)
+                cv2.putText(img, hand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+                            2, drawBboxColor, 2)
+        return img
+    return f
+
+# Draw finger tip
+def drawFingerTip(obj):
+    def f(self, img, drawFingerTip=False, drawFingerTipRadius=15,
+          drawFingerTipColor=(255, 0, 255), **kwargs):
+        if self.resultsData and drawFingerTip:
+            for hand in self.allHands:
+                lmList = hand["lmList"]
+                for idlm in HandEnum.tips():
+                    (cx, cy, cz) = lmList[idlm]
+                    cv2.circle(img, (cx, cy),
+                                drawFingerTipRadius,
+                                drawFingerTipColor, cv2.FILLED)
+                    # if cz < 0:
+                        # cv2.putText(img, str(cz), (cx - 30, cy - 30), cv2.FONT_HERSHEY_PLAIN,
+                        #             2, drawBboxColor, 2)
+        return img
+    return f
+
+def drawCommands(obj):
+    def f(self, img, drawCommand=False, drawCommandLine=3, **kwargs):
+        if self.resultsData and drawCommand:
+            rHand = self.getHandsInfo(handNo="Right")
+            if rHand:
+                #(cx, cy) = rHand["center"]
+                (d1x, d1y, d1z) = rHand["lmList"][HandEnum.WRIST.value]
+                (d2x, d2y, d2z) = rHand["lmList"][HandEnum.PINKY_MCP.value]
+                (cx, cy, cz) = rHand["lmList"][HandEnum.INDEX_FINGER_MCP.value]
+                (px, py, pz) = rHand["lmList"][HandEnum.INDEX_FINGER_TIP.value]
+
+                minDist = max(math.dist((d1x, d1y), (d2x, d2y)), math.dist((cx, cy), (d2x, d2y)))
+
+                distance = math.dist((cx, cy), (px, py))
+                angle = math.degrees(math.atan2(py-cy, px-cx))
+                print(angle)
+                drawCommandColor = (0, 0, 0)
+                delta = 15
+
+                if distance > minDist:
+                    if (-45+delta) < angle and angle < (45-delta):
+                        drawCommandColor = (255, 0, 0) # Blue -> left
+                    elif (45+delta) < angle and angle < (135-delta):
+                        drawCommandColor = (0, 255, 0) # Green -> bottom
+                    elif (-135+delta) < angle and angle < (-45-delta):
+                        drawCommandColor = (0, 0, 255) # Red -> top
+                    elif (135+delta) < angle or angle < (-135-delta):
+                        drawCommandColor = (255, 0, 255) # magenta -> right
+
+                cv2.line(img, (cx, cy), (px, py), drawCommandColor, drawCommandLine)
+
+        return img
+    return f
+
+
+# def __(obj):
+#     def f(self, img, **kwargs):
+#         return img
+#     return f
+
+
+    # def drawHands(self, img,
+    #               drawFps=False, drawFpsColor=(255, 0, 255), # BGR
+    #               drawHand=False,
+    #               drawBbox=False, drawBboxColor=(255, 0, 255), # BGR
+    #               drawFingerTip=False, drawFingerTipRadius=15,
+    #               drawFingerTipColor=(255, 0, 255), # BGR
+    #               ):
+
+    #     if drawFps:
+    #         self.cTime = time.time();
+    #         fps = 1/(self.cTime - self.pTime)
+    #         self.pTime = self.cTime
+
+    #         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+    #                     drawFpsColor, 3)
+
+    #     if self.results.multi_hand_landmarks:
+    #         # Draw hand connections
+    #         if drawHand:
+    #             for handLms in self.results.multi_hand_landmarks:
+    #                     self.mpDraw.draw_landmarks(img, handLms,
+    #                                                self.mpHands.HAND_CONNECTIONS)
+
+    #         # Draw Bbox
+    #         if drawBbox:
+    #             for hand in self.allHands:
+    #                 bbox = hand["bbox"]
+    #                 cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+    #                               (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+    #                               drawBboxColor, 2)
+    #                 cv2.putText(img, hand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+    #                             2, drawBboxColor, 2)
+
+    #         # Draw finger tip
+    #         if drawFingerTip:
+    #             for hand in self.allHands:
+    #                 lmList = hand["lmList"]
+    #                 for idlm in HandEnum.tips():
+    #                     (cx, cy, cz) = lmList[idlm]
+    #                     if cz < 0:
+    #                         cv2.circle(img, (cx, cy),
+    #                                    drawFingerTipRadius,
+    #                                    drawFingerTipColor, cv2.FILLED)
+
+    #                         # cv2.putText(img, str(cz), (cx - 30, cy - 30), cv2.FONT_HERSHEY_PLAIN,
+    #                         #             2, drawBboxColor, 2)
+    #     return img
+
