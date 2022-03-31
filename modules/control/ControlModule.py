a5a29a5ab348e7acc339a0877a4763b25f70305a
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-03-31 18:36:21 +0200

    added commands

--- modules/control/ControlModule.py
+++ modules/control/ControlModule.py
@@ -9,6 +9,20 @@ from modules.drone.DroneModule import Drone
 class Command(Enum):
     TAKE_OFF = 1
     LAND = 2
+    STREAM_ON = 3
+    STREAM_OFF = 4
+
+    MOVE_FORWARD = 10
+
+    MOVE_BACKWARD = 20
+
+    ROTATE_CW = 30
+
+    ROTATE_CCW = 40
+
+    FOLLOW_ME = 100
+
+    STOP_EXECUTION = 1000
 
 
 class ControlModule(ABC):
@@ -20,16 +34,33 @@ class ControlModule(ABC):
             self._drone.take_off()
         elif command == Command.LAND:
             self._drone.land()
+        elif command == Command.STREAM_ON:
+            self._drone
+        elif command == Command.STREAM_OFF:
+            self._drone
+
+        elif command == Command.MOVE_FORWARD:
+            self._drone
+
+        elif command == Command.MOVE_BACKWARD:
+            self._drone
+
+        elif command == Command.ROTATE_CW:
+            self._drone
+
+        elif command == Command.ROTATE_CCW:
+            self._drone
+
+        elif command == Command.FOLLOW_ME:
+            self._drone
+
+        elif command == Command.STOP_EXECUTION:
+            self._drone
+
 
     def execute(self, command: Command):
         Thread(target=self._execute, args=[command]).start()
 
     def end(self):
-        # try:
-        #     self._drone.land()
-        # except Exception:
-        #     # Drone has just landed
-        #     pass
-
         self._drone.end()
 
