33cf29442148bb4f1aa5249abd8aef023c9de259
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-03-31 19:06:30 +0200

    corrected except error

--- modules/stream/AudioStreamModule.py
+++ modules/stream/AudioStreamModule.py
@@ -28,18 +28,22 @@ class ComputerMicrophoneStream(AudioStreamModule):
         self.listener = sr.Recognizer()
 
     def get_stream_word(self):
-        with sr.Microphone() as source:
-            print("Listening...")
-            voice = self.listener.listen(source, timeout=5, phrase_time_limit=5)
-            try:
-                command = self.listener.recognize_google(voice)
-            except speech_recognition.UnknownValueError:
-                return ""
-            command = command.lower()
-            if self.name in command:
-                command = command.replace(self.name, '')
-            else:
-                command = ''
+        command = ''
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                voice = self.listener.listen(source, timeout=5, phrase_time_limit=5)
+                try:
+                    command = self.listener.recognize_google(voice)
+                except speech_recognition.UnknownValueError:
+                    return ""
+                command = command.lower()
+                if self.name in command:
+                    command = command.replace(self.name, '')
+                else:
+                    command = ''
+        except:
+            pass
         return command
 
     def release_stream(self):
