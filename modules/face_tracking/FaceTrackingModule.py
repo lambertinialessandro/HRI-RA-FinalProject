a5a05a41bdc9c485e2c328cb03c937d530bb3b8a
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2022-04-03 16:38:45 +0200

    Added custom BBox object in FaceTrackingModule

--- modules/face_tracking/FaceTrackingModule.py
+++ modules/face_tracking/FaceTrackingModule.py
@@ -1,6 +1,8 @@
+import dataclasses
 
 import cv2
 import mediapipe as mp
+
 
 class FaceDetector:
     def __init__(self, model_selection=1, min_detection_confidence=.5):
@@ -13,6 +15,7 @@ class FaceDetector:
 
         self.all_bboxes = []
         self.results_data = False
+        self.results = None
 
     def analyze_frame(self, frame):
         self.results = self.faceDetection.process(frame)
@@ -26,20 +29,30 @@ class FaceDetector:
         # collecting infos
         h, w, c = frame.shape
         for id, detection in enumerate(self.results.detections):
-            bboxC = detection.location_data.relative_bounding_box
-            bbox = int(bboxC.xmin * w), int(bboxC.ymin * h), \
-                   int(bboxC.width * w), int(bboxC.height * h)
+            bbox_c = detection.location_data.relative_bounding_box
+            bbox = FaceDetector.BBox(top_left_corner=(int(bbox_c.xmin * w), int(bbox_c.ymin * h)),
+                                     size=(int(bbox_c.width * w), int(bbox_c.height * h)))
             self.all_bboxes.append(bbox)
 
-            cv2.rectangle(frame, bbox, (255, 0, 255), 2)
+            cv2.rectangle(frame, bbox.to_tuple, (255, 0, 255), 2)
             cv2.putText(frame, f'{int(detection.score[0]*100)}%',
-                       (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN,
-                       2, (255, 0, 255), 2)
+                        (bbox.top_left_corner[0], bbox.top_left_corner[1]-20), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 255), 2)
         return frame
+
+    @dataclasses.dataclass
+    class BBox:
+        top_left_corner: tuple
+        size: tuple
+
+        @property
+        def to_tuple(self) -> tuple:
+            return self.top_left_corner[0], self.top_left_corner[1], self.size[0], self.size[1]
+
 
 def main():
     try:
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        cap = cv2.VideoCapture(0)
         cap.set(3, 1280//2)
         cap.set(4, 720//2)
         detector = FaceDetector(min_detection_confidence=.8)
