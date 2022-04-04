36277e19308bb8e9166c2cc1a58dd5008da8701c
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-04 16:07:13 +0200

    backward and forward motion

--- modules/face_tracking/FaceTrackingModule.py
+++ modules/face_tracking/FaceTrackingModule.py
@@ -10,22 +10,8 @@ sys.path.append('../../')
 
 from modules.control.ControlModule import Command
 
-class FaceTracking:
-    def __init__(self, min_detection_confidence=0.5,
-                x_P=0.7, x_I=0.01, x_D=0.05,
-                y_P=0.7, y_I=0.01, y_D=0.05,
-                sample_time=0.01):
-        
-        self.face_detection = mp.solutions.face_detection.FaceDetection(
-                model_selection=model_selection,
-                min_detection_confidence=min_detection_confidence)
-        
-        self.__pid_x = PID(x_P, x_I, x_D, sample_time=sample_time)
-        self.__pid_y = PID(y_P, y_I, y_D, sample_time=sample_time)
+class AbstractFaceTracking(ABC):
 
-        self.old_control_x = None
-        self.old_control_y = None
-    
     @dataclasses.dataclass
     class __BBox:
         x: int
@@ -41,7 +27,14 @@ class FaceTracking:
         def to_tuple(self) -> tuple:
             return self.x, self.y, self.w, self.h
 
-    def __analyze_frame(self, frame):
+    def __init__(self, model_selection=1,
+                 min_detection_confidence=0.5):
+
+        self.face_detection = mp.solutions.face_detection.FaceDetection(
+                model_selection=model_selection,
+                min_detection_confidence=min_detection_confidence)
+
+    def _analyze_frame(self, frame):
         h, w, c = frame.shape
         center = w//2, h//2
         results = self.face_detection.process(frame)
@@ -67,31 +60,58 @@ class FaceTracking:
 
         cv2.circle(frame, center, 5, (0, 0, 255), cv2.FILLED)
         return all_bboxes
-    
+
+    @classmethod
+    @abstractmethod
+    def execute(self, frame) -> tuple:
+        pass
+
+class FaceTracking(AbstractFaceTracking):
+    def __init__(self, model_selection=1, min_detection_confidence=0.5,
+                sample_time=0.01, box_width=100):
+        super().__init__(model_selection=model_selection,
+                         min_detection_confidence=min_detection_confidence)
+
+        self._pid_x = PID(0.7, 0.01, 0.05, sample_time=sample_time)
+        self._pid_y = PID(0.7, 0.01, 0.05, sample_time=sample_time)
+
+        self.box_width = box_width
+        self._pid_z = PID(0.7, 0.01, 0.05, sample_time=sample_time)
+
+        self.old_control_x = None
+        self.old_control_y = None
+        self.old_control_z = None
+
     #TODO
     def execute(self, frame) -> tuple:
         goal_y = frame.shape[0] // 2
         goal_x = frame.shape[1] // 2
+        goal_z = self.box_width
 
-        self.__pid_x.setpoint = goal_x
-        self.__pid_y.setpoint = goal_y
+        self._pid_x.setpoint = goal_x
+        self._pid_y.setpoint = goal_y
 
-        bboxes = self.__analyze_frame(frame)
+        bboxes = self._analyze_frame(frame)
 
         if len(bboxes) > 0:
             face = bboxes[0]
             face_center = face.center
 
-            control_x = self.__pid_x(face_center[0])
-            control_y = self.__pid_y(face_center[1])
+            control_x = self._pid_x(face_center[0])
+            control_y = self._pid_y(face_center[1])
+            control_z = self._pid_y(face.w)
 
-            if control_x == self.old_control_x and control_y == self.old_control_y:
+            if control_x == self.old_control_x and \
+                    control_y == self.old_control_y and\
+                    control_z == self.old_control_z:
                 return Command.NONE, None
 
             if control_x != self.old_control_x:
                 self.old_control_x = control_x
             if control_y != self.old_control_y:
                 self.old_control_y = control_y
+            if control_z != self.old_control_z:
+                self.old_control_z = control_z
 
             control_x /= goal_x
             control_x *= -100
@@ -99,7 +119,10 @@ class FaceTracking:
             control_y /= goal_y
             control_y *= 100
 
-            control = (0, 0, int(control_y), int(control_x))
+            control_z /= goal_z
+            control_z *= 100
+
+            control = (0, int(control_z), int(control_y), int(control_x))
 
             return Command.SET_RC, control
         else:
