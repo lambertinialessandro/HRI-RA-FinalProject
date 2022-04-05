64b93b693446097535f5380de3d0d397c5a0fe65
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-05 12:12:03 +0200

    moved HandEnum inside HandTrackingModule.py

--- modules/command_recognition/hand_tracking/HandTrackingModule.py
+++ modules/command_recognition/hand_tracking/HandTrackingModule.py
@@ -1,6 +1,7 @@
 
 import cv2
 import math
+from enum import Enum
 import mediapipe as mp
 
 # TODO
@@ -11,6 +12,33 @@ sys.path.append('../../../')
 from modules.control.ControlModule import Command
 
 
+class HandEnum(Enum):
+    WRIST = 0
+    THUMB_CMC = 1
+    THUMB_MCP = 2
+    THUMB_IP = 3
+    THUMB_TIP = 4
+    INDEX_FINGER_MCP = 5
+    INDEX_FINGER_PIP = 6
+    INDEX_FINGER_DIP = 7
+    INDEX_FINGER_TIP = 8
+    MIDDLE_FINGER_MCP = 9
+    MIDDLE_FINGER_PIP = 10
+    MIDDLE_FINGER_DIP = 11
+    MIDDLE_FINGER_TIP = 12
+    RING_FINGER_MCP = 13
+    RING_FINGER_PIP = 14
+    RING_FINGER_DIP = 15
+    RING_FINGER_TIP = 16
+    PINKY_MCP = 17
+    PINKY_PIP = 18
+    PINKY_DIP = 19
+    PINKY_TIP = 20
+
+    def tips(self):
+        return [self.THUMB_TIP.value, self.INDEX_FINGER_TIP.value,
+                self.MIDDLE_FINGER_TIP.value, self.RING_FINGER_TIP.value,
+                self.PINKY_TIP.value]
 
 
 class HandDetector:
