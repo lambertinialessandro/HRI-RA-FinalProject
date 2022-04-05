a6e0832c0d3017f308253824cdc7f6a849deb43e
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-05 12:13:27 +0200

    added keyboard input to quit

--- modules/command_recognition/AudioCommandRecognitionModule.py
+++ modules/command_recognition/AudioCommandRecognitionModule.py
@@ -28,35 +28,48 @@ class AudioCommandRecognition(AbstractAudioCommandRecognition):
         voices = self.engine.getProperty('voices')
         self.engine.setProperty('voice', voices[1].id)
 
-    def __talk(self, text):
+        self.done = False
+        def my_keyboard_hook(keyboard_event):
+            #print("Name:", keyboard_event.name)
+            #print("Scan code:", keyboard_event.scan_code)
+            #print("Time:", keyboard_event.time)
+
+            if keyboard_event.scan_code == 1: # Esc
+                print("Esc pressed!")
+                self.done = True
+
+        keyboard.hook(my_keyboard_hook)
+
+    def _talk(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
 
     def get_command(self, text) -> tuple:
         print("Executing: "+text)
         # if 'start video' in command:
-        #     self.__talk("Video started!")
+        #     self._talk("Video started!")
         #     return Command.STREAM_ON
         # elif 'stop video' in command:
-        #     self.__talk("Video stopped!")
+        #     self._talk("Video stopped!")
         #     return Command.STREAM_OFF
         # el
-        if 'take off' in text:
-            self.__talk("Starting drone! wrwrwr")
+        if self.done or 'turn off' in text:
+            self._talk("Stop execution")
+            keyboard.unhook_all()
+            return Command.STOP_EXECUTION, None
+        elif 'take off' in text:
+            self._talk("Starting drone! wrwrwr")
             return Command.TAKE_OFF, None
         elif 'land' in text:
-            self.__talk("Stopping drone! wrwrwr")
+            self._talk("Stopping drone! wrwrwr")
             return Command.LAND, None
         elif 'follow me' in text:
-            self.__talk("i'm following")
+            self._talk("i'm following")
             return Command.FOLLOW_ME, None
-        elif 'turn off' in text:
-            self.__talk("Stop execution")
-            return Command.STOP_EXECUTION, None
         else:
-            self.__talk('Please say the command again.')
+            self._talk('Please say the command again.')
 
-        return None, None
+        return Command.NONE, None
 
 
 # TODO
