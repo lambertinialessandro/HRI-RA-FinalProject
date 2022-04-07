0c390dd4e8592894575118d0255a4409dc578196
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2022-04-07 16:24:02 +0200

    Aligned with as in main

--- modules/window/Window.py
+++ modules/window/Window.py
@@ -1,50 +1,29 @@
 import cv2
-import schedule
+
+from modules.DrawerModule import PipelineDrawerBuilder
 
 
 class Window:
-    instance = None
-
-    def __init__(self, get_battery, get_fps, on_closed, name="Video"):
-        Window.instance = self
-
-        self.get_battery = get_battery
-        self.get_fps = get_fps
+    def __init__(self, drone, on_closed, name="Video"):
         self.on_closed = on_closed
         self.name = name
 
-        self.frame = None
+        self.pd = PipelineDrawerBuilder.build(drone,
+                                              [PipelineDrawerBuilder.DRAWER_FPS,
+                                               PipelineDrawerBuilder.DRONE_BATTERY,
+                                               PipelineDrawerBuilder.DRONE_TEMPERATURE,
+                                               PipelineDrawerBuilder.DRONE_HEIGHT])
 
         cv2.namedWindow(self.name)
 
-        self.battery = None
-        self._update_battery()
-        schedule.every(10).seconds.do(self._update_battery)
-
-    def _update_battery(self):
-        self.battery = self.get_battery()
-
-    def write(self, text, position=(0, 0), font_scale=1, color=(0, 0, 0), thickness=1):
-        cv2.putText(self.frame, text, position, cv2.FONT_HERSHEY_PLAIN, font_scale, color, thickness)
-
-    def draw_circle(self, center, radius, color=(0, 0, 0), thickness=1):
-        cv2.circle(self.frame, center, radius, color, thickness, cv2.FILLED)
-
-    def draw_rectangle(self, x, y, w, h, color=(0, 0, 0), thickness=1):
-        cv2.rectangle(self.frame, (x, y), (w, h), color, thickness)
-
     def show(self, frame):
-        self.frame = frame
-        self.write(f"Battery: {self.battery}%", position=(10, 15), color=(0, 0, 255))
-        self.write(f"FPS: {self.battery}%", position=(10, 30), color=(0, 0, 255))
+        self.pd.draw(frame)
 
         cv2.imshow(self.name, frame)
 
         key = cv2.waitKey(1)
         if key == 27:  # ESC
             self.on_closed()
-            self.destroy()
 
     def destroy(self):
-        Window.instance = None
         cv2.destroyAllWindows()
