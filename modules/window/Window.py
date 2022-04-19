d0e6c3af6764252c88724e80bd4f56c4d30f08a0
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-19 11:11:33 +0200

    Update Window.py

--- modules/window/Window.py
+++ modules/window/Window.py
@@ -2,48 +2,48 @@ import cv2
 import keyboard
 
 from modules.DrawerModule import PipelineDrawerBuilder
+from modules.command_recognition.CommandRecognitionFactory import CommandRecognitionFactory
 
 
 class Window:
     instance = None
 
-    def __init__(self, drone, on_closed, name="Video"):
-        self.on_closed = on_closed
-        self.name = name
+    def __init__(self, cls, name="Video"):
+        self.cls = cls
 
-        self.pd = PipelineDrawerBuilder.build(drone,
+        self.pd = PipelineDrawerBuilder.build(cls.drone,
                                               [PipelineDrawerBuilder.DRAWER_FPS,
                                                PipelineDrawerBuilder.DRONE_BATTERY,
                                                PipelineDrawerBuilder.DRONE_TEMPERATURE,
                                                PipelineDrawerBuilder.DRONE_HEIGHT])
 
-        cv2.namedWindow(self.name)
+        #cv2.namedWindow(name)
 
         def my_keyboard_hook(keyboard_event):
-            print("Name:", keyboard_event.name)
-            print("Scan code:", keyboard_event.scan_code)
-            print("Time:", keyboard_event.time)
+            # print("Name:", keyboard_event.name)
+            # print("Scan code:", keyboard_event.scan_code)
+            # print("Time:", keyboard_event.time)
 
             if keyboard_event.name == "t":
-                drone.take_off()
+                cls.drone.take_off()
             elif keyboard_event.name == "l":
-                drone.land()
+                cls.drone.land()
             elif keyboard_event.name == "u":
-                drone.move_up(10)
+                cls.drone.move_up(10)
             elif keyboard_event.name == "d":
-                drone.move_down(10)
+                cls.drone.move_down(10)
 
-            # if keyboard_event.name == "1":
-            #     print("Face!")
-            #     self.update_detector(VideoTrackingFactory.Face)
-            # elif keyboard_event.name == "2":
-            #     print("Hand!")
-            #     self.update_detector(VideoTrackingFactory.Hand)
+            elif keyboard_event.name == "1":
+                print("Face!")
+                cls.command_recognition = CommandRecognitionFactory.create(CommandRecognitionFactory.VideoFace)
+            elif keyboard_event.name == "2":
+                print("Hand!")
+                cls.command_recognition = CommandRecognitionFactory.create(CommandRecognitionFactory.VideoHand)
             # elif keyboard_event.name == "3":
             #     print("Holistic!")
             #     self.update_detector(VideoTrackingFactory.Holistic)
 
-        keyboard.hook(my_keyboard_hook)
+        keyboard.on_press(my_keyboard_hook)
 
         Window.instance = self
 
@@ -56,7 +56,7 @@ class Window:
         if key == 27:  # ESC
             self.on_closed()
 
-    def destroy(self):
+    def end(self):
         Window.instance = None
 
         keyboard.unhook_all()
