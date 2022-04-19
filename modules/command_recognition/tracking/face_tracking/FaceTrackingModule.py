ffede6d74bd00a03fd0253029c39a3519e50dd28
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-04-19 11:10:06 +0200

    Update FaceTrackingModule.py

--- modules/command_recognition/tracking/face_tracking/FaceTrackingModule.py
+++ modules/command_recognition/tracking/face_tracking/FaceTrackingModule.py
@@ -17,41 +17,10 @@ class AbstractFaceTracking(AbstractModuleTracking):
     def __init__(self, model_selection=1, min_detection_confidence=0.5):
         super().__init__()
 
+        self.bboxes = []
         self.face_detection = mp.solutions.face_detection.FaceDetection(
                 model_selection=model_selection,
                 min_detection_confidence=min_detection_confidence)
-
-    def analyze_frame(self, frame):
-        h, w, _ = frame.shape
-        results = self.face_detection.process(frame)
-
-        all_bboxes = []
-        if not results.detections:
-            return all_bboxes
-
-        # collecting infos
-        for id, detection in enumerate(results.detections):
-            bbox_c = detection.location_data.relative_bounding_box
-            bbox = self._FaceBBox(x=bbox_c.xmin,
-                                  y=bbox_c.ymin,
-                                  w=bbox_c.width,
-                                  h=bbox_c.height,
-                                  detection=detection.score[0])
-            center = bbox.center
-
-            # Window.instance.draw_circle((center[0] * w, center[1] * h), 2, (0, 255, 0))
-            # Window.instance.draw_rectangle(*bbox.to_tuple(), color=(255, 0, 255), thickness=2)
-            # Window.instance.write(f"{int(detection.score[0]*100)}%", position=(bbox.x, bbox.y-20), font_scale=2,
-            #                       color=(255, 0, 255), thickness=2)
-
-            all_bboxes.append(bbox)
-
-        # Window.instance.draw_circle(center, 5, (0, 0, 255))
-        return all_bboxes
-
-    @abstractmethod
-    def execute(self, frame) -> tuple:
-        pass
 
     @dataclasses.dataclass
     class _FaceBBox:
@@ -73,7 +42,6 @@ class AbstractFaceTracking(AbstractModuleTracking):
             return self.x, self.y, self.w, self.h
 
         def to_unnormalized_tuple(self, shape) -> tuple:
-            normalized_center = self.center
             return int(self.x * shape[0]), int(self.y * shape[1]), \
                 int(self.w * shape[0]), int(self.h * shape[1]),
 
@@ -84,34 +52,61 @@ class AbstractFaceTracking(AbstractModuleTracking):
             self.y /= height
             self.h /= height
 
+    def _analyze_frame(self, frame):
+        h, w, _ = frame.shape
+        results = self.face_detection.process(frame)
+
+        self.bboxes = []
+        if not results.detections:
+            return
+
+        # collecting infos
+        for id, detection in enumerate(results.detections):
+            bbox_c = detection.location_data.relative_bounding_box
+            bbox = self._FaceBBox(x=bbox_c.xmin,
+                                  y=bbox_c.ymin,
+                                  w=bbox_c.width,
+                                  h=bbox_c.height,
+                                  detection=detection.score[0])
+            self.bboxes.append(bbox)
+
+    @abstractmethod
+    def _execute(self) -> tuple:
+        pass
+
+    @abstractmethod
+    def edit_frame(self, frame):
+        pass
+
+    def end(self):
+        pass
+
 
 import cv2
 
 class PIDFaceTracking(AbstractFaceTracking):
-    def __init__(self, model_selection=1, min_detection_confidence=0.5, sample_time=0.01, box_width=100):
+    def __init__(self, model_selection=1, min_detection_confidence=0.5, sample_time=0.01):
         super().__init__(model_selection=model_selection, min_detection_confidence=min_detection_confidence)
 
-        self._pid_x = PID(0.7, 0.01, 0.05, sample_time=sample_time, setpoint=0.5)
-        self._pid_y = PID(0.7, 0.01, 0.05, sample_time=sample_time, setpoint=0.5)
-
-        self.box_width = box_width
-        self._pid_z = PID(0.7, 0.01, 0.05, sample_time=sample_time)
+        # 0.7, 0.01, 0.05
+        self._pid_x = PID(0.7, 0.1, 0.05, sample_time=sample_time, setpoint=0.5)
+        self._pid_y = PID(0.7, 0.1, 0.05, sample_time=sample_time, setpoint=0.5)
+        self._pid_z = PID(0.7, 0.1, 0.05, sample_time=sample_time, setpoint=0.2)
 
         self.old_control_x = None
         self.old_control_y = None
         self.old_control_z = None
 
-    def execute(self, bboxes) -> tuple:
-        goal_z = self.box_width  # TODO
-
-        if len(bboxes) > 0:
-            face = bboxes[0]
+    def _execute(self) -> tuple:
+        control = (0, 0, 0, 0)
+        if len(self.bboxes) > 0:
+            face = self.bboxes[0]
             face_center = face.center
-            print(face_center)
 
             control_x = self._pid_x(face_center[0])
+            print(face_center[1])
             control_y = self._pid_y(face_center[1])
-            control_z = self._pid_y(face.w)
+            control_z = self._pid_z(face.w)
 
             if control_x == self.old_control_x and \
                     control_y == self.old_control_y and\
@@ -126,35 +121,42 @@ class PIDFaceTracking(AbstractFaceTracking):
                 self.old_control_z = control_z
 
             control_x *= -100
-
             control_y *= 100
+            control_z *= -100
 
-            # control_z /= goal_z
-            control_z *= 100
+            control_z = control_z if 0.1 < face.w and face.w < 0.3 else 0
 
             control = (0, int(control_z), int(control_y), int(control_x))
+            print(control)
 
-            return Command.SET_RC, control
-        else:
-            return Command.SET_RC, (0, 0, 0, 0)
+        return Command.SET_RC, control
 
-    def edit(self, frame, bboxes):
+    def edit_frame(self, frame):
         h, w, _ = frame.shape
         shape = (w, h)
         center = w//2, h//2
 
-        for bbox in bboxes:
-            cv2.circle(frame, bbox.unnormalized_center(shape), 2, (0, 255, 0), cv2.FILLED)
+        for bbox in self.bboxes:
+            cv2.circle(frame, bbox.unnormalized_center(shape),
+                       2, (0, 255, 0), cv2.FILLED)
             cv2.rectangle(frame, bbox.to_unnormalized_tuple(shape), (255, 0, 255), 2)
             cv2.putText(frame, f'{int(bbox.detection*100)}%',
                         (int(bbox.x*w), int(bbox.y*h-20)), cv2.FONT_HERSHEY_PLAIN,
                         2, (255, 0, 255), 2)
+            cv2.putText(frame, f'{bbox.w}',
+                        (int(bbox.x*w), int(bbox.y*h-35)), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 255), 2)
+
+        if len(self.bboxes) > 0:
+            cv2.line(frame, center, self.bboxes[0].unnormalized_center(shape),
+                     (0, 0, 0), thickness=1)
+            cv2.line(frame, center,(
+                            int(center[0] - self.old_control_x*360),
+                            int(center[1] - self.old_control_y*360)
+                        ), (255, 0, 0), thickness=2)
 
         cv2.circle(frame, center, 5, (0, 0, 255), cv2.FILLED)
         return frame
-
-    def end(self):
-        pass
 
 def main():
     import cv2
