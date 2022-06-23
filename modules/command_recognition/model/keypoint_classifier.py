387778ec31be81b0d61678073c863eed11dc73e4
Gianluca Lofrumento <gianluca.lofrumento@gmail.com>
2022-06-23 23:20:50 +0200

    Updated KeypointClassifier

--- modules/command_recognition/model/keypoint_classifier.py
+++ modules/command_recognition/model/keypoint_classifier.py
@@ -9,54 +9,54 @@ class KeyPointClassifier(object):
     def __init__(
         self,
         model_path='modules/command_recognition/model/keypoint_classifier.tflite',
-        num_threads=1,
     ):
-        self.interpreter = tf.lite.Interpreter(model_path=model_path,
-                                               num_threads=num_threads)
+        self.interpreter = tf.lite.Interpreter(
+            model_path=model_path,
+            num_threads=1
+        ).allocate_tensors()
 
-        self.interpreter.allocate_tensors()
-        self.input_details = self.interpreter.get_input_details()
-        self.output_details = self.interpreter.get_output_details()
+        self.input = self.interpreter.get_input_details()
+        self.output = self.interpreter.get_output_details()
 
-    def classify(self, landmark_list):
-        landmark_list = self._pre_process_landmark(landmark_list)
+    def classify(self, landmarks):
+        landmarks = np.array([self._pre_process_landmark(landmarks)], dtype=np.float32)
 
-        input_details_tensor_index = self.input_details[0]['index']
+        input_details_tensor_index = self.input[0]['index']
         self.interpreter.set_tensor(
             input_details_tensor_index,
-            np.array([landmark_list], dtype=np.float32))
+            landmarks,
+        )
         self.interpreter.invoke()
 
-        output_details_tensor_index = self.output_details[0]['index']
+        output_index = self.output[0]['index']
 
-        result = self.interpreter.get_tensor(output_details_tensor_index)
+        result = np.argmax(
+            np.squeeze(
+                self.interpreter.get_tensor(output_index)
+            )
+        )
 
-        result_index = np.argmax(np.squeeze(result))
+        return result
 
-        return result_index
-
-    def _pre_process_landmark(self, landmark_list):
-        temp_landmark_list = copy.deepcopy(landmark_list)
+    def _pre_process_landmark(self, landmarks):
+        temp_landmarks = copy.deepcopy(landmarks)
 
         # Convert to relative coordinates
         base_x, base_y = 0, 0
-        for index, landmark_point in enumerate(temp_landmark_list):
+        for index, landmark_point in enumerate(temp_landmarks):
             if index == 0:
                 base_x, base_y = landmark_point[0], landmark_point[1]
 
-            temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
-            temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
-            temp_landmark_list[index].pop()
+            temp_landmarks[index][0] = temp_landmarks[index][0] - base_x
+            temp_landmarks[index][1] = temp_landmarks[index][1] - base_y
+            temp_landmarks[index].pop()
 
         # Convert to a one-dimensional list
-        temp_landmark_list = list(itertools.chain.from_iterable(temp_landmark_list))
+        temp_landmarks = list(itertools.chain.from_iterable(temp_landmarks))
 
         # Normalization
-        max_value = max(list(map(abs, temp_landmark_list)))
+        max_value = max(list(map(abs, temp_landmarks)))
 
-        def normalize_(n):
-            return n / max_value
+        temp_landmarks = list(map(lambda n: n / max_value, temp_landmarks))
 
-        temp_landmark_list = list(map(normalize_, temp_landmark_list))
-
-        return temp_landmark_list
+        return temp_landmarks
