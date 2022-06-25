3ea09bb0cd433e596eb42e7d9aeb1ef545b7dd0e
Alessandro Lambertini <lambertinialessandro16@gmail.com>
2022-06-25 20:02:55 +0200

    Update RecordVideoModule.py

--- modules/stream/RecordVideoModule.py
+++ modules/stream/RecordVideoModule.py
@@ -7,7 +7,6 @@ class RecordVideoModule:
     def __init__(self, filename, size=(1920, 1080)):
         self.video = imageio.get_writer(os.path.join("video_out", filename),
                                         fps=24)
-
         self.is_running = True
 
     def write_frame(self, frame):
